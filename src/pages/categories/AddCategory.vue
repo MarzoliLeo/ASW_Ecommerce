61743505c8360485f8ea51a080daeb96705a2e31
Leo_Marzoli <leo.marzoli@studio.unibo.it>
2023-07-28 19:40:55 +0200

    Aggiunto form per categorizzare i corsi

---
+++ src/pages/categories/AddCategory.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+
+  <div class="container">
+    <div class="row">
+        <div class = "text-center">
+          <h3>Add Category</h3>
+        </div>
+    </div>
+    <div class ="row">
+      <!--Utilizzo bootstrap per creare il form-->
+      <form>
+        <div class="form-group">
+          <label>Category Name</label>
+          <input type="text" class="form-control">
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,13 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HomePage from "@/pages/HomePage.vue"
 import AboutPage from "@/pages/AboutPage.vue"
+import AddCategory from "@/pages/categories/AddCategory.vue"
 import NotFound from "@/pages/NotFound.vue"
 
 const routes = [
   { path: "/", name: "Home", component: HomePage },
   { path: "/about", name: "About", component: AboutPage },
+  { path: "/admin/category/add", name: "addCategory", component: AddCategory},
   { path: "/404", name: "NotFound", component: NotFound },
-  { path: "/:catchAll(.*)", redirect: "/404" },
+  { path: "/:catchAll(.*)", redirect: "/404" }
 ]
 
 const router = createRouter({
