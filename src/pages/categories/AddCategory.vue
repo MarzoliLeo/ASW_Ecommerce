78af9f44ffe4d2b77e999f907c75d3e2df73f6d9
Leo_Marzoli <leo.marzoli@studio.unibo.it>
2023-07-28 19:48:34 +0200

    Sistamato l'aspetto del Form.

--- src/pages/categories/AddCategory.vue
+++ src/pages/categories/AddCategory.vue
@@ -6,17 +6,30 @@
   <div class="container">
     <div class="row">
         <div class = "text-center">
-          <h3>Add Category</h3>
+          <h3 class="pt-3">Add Category</h3>
         </div>
     </div>
     <div class ="row">
       <!--Utilizzo bootstrap per creare il form-->
-      <form>
-        <div class="form-group">
-          <label>Category Name</label>
-          <input type="text" class="form-control">
-        </div>
-      </form>
+      <div class="col-3"></div>
+      <div class="col-6">
+        <form>
+          <div class="form-group">
+            <label>Category Name</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="form-group">
+            <label>Category Description</label>
+            <input type="text" class="form-control">
+          </div>
+          <div class="form-group">
+            <label>Category Image</label>
+            <input type="text" class="form-control">
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      <div class="col-3"></div>
     </div>
   </div>
 </template>
