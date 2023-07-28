de2b6572facec711d31924bc8500656e1576e73a
Leo_Marzoli <leo.marzoli@studio.unibo.it>
2023-07-28 21:41:21 +0200

    Aggiunta l'integrazione fra Axios e Postman.

--- src/pages/categories/AddCategory.vue
+++ src/pages/categories/AddCategory.vue
@@ -1,10 +1,34 @@
 <script>
+const axios = require("axios");
+//Queste variabili verranno utilizzate nel backend tramite Axios.
 export default{
   data() {
     return {
       categoryName: "",
       description: "",
       imageUrl: "",
+    }
+  },
+  methods: {
+    addCategory() {
+      console.log(this.categoryName, this.description);
+        //utilizziamo Axios(API Web) per creare un oggetto categoria.
+        const newCategory = {
+          categoryName: this.categoryName,
+          description: this.description,
+          imageUrl: this.imageUrl,
+        };
+
+        axios({
+          method: 'post',
+          //Utilizzo POSTMAN per verificare che le mie API siano corrette.
+          url: `postman-echo.com/get?foo=bar`,
+          data: JSON.stringify(newCategory),
+          hearders: {
+            'Content-Type': 'application/json'
+          }
+        }).then(() => {})
+        .catch(err => console.log(err));
     }
   }
 }
@@ -37,7 +61,7 @@ export default{
           </div>
           <br>
           <div class="text-center">
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="button" class="btn btn-primary" @click="addCategory">Submit</button>
           </div>
         </form>
       </div>
